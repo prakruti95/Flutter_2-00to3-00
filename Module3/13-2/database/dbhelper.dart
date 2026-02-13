44f80790c7d949e90453b72bdee98a29197bfece
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2026-02-13 14:51:11 +0530

    commit

---
+++ Module3/13-2/database/dbhelper.dart
@@ -0,0 +1,56 @@
+import 'dart:io';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
+class MyDb
+{
+  //late Database db;
+  static final _databaseName = "topstech.db";
+  static final _databaseVersion = 1;
+  static final table = 'category';
+  static final columnId = '_id';
+  static final columnname = 'category_name';
+  static Database? _database;
+  MyDb._privateConstructor();
+  static final MyDb instance = MyDb._privateConstructor();
+
+  Future <void> _onCreate(Database db, int version) async
+  {
+    await db.execute('''
+          CREATE TABLE $table (
+            $columnId INTEGER PRIMARY KEY,
+            $columnname TEXT NOT NULL 
+          )
+          ''');
+  }
+
+  Future<Database> get database async => _database ??= await _initDatabase();
+
+  _initDatabase()async
+  {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, _databaseName);
+    return await openDatabase
+      (path, version: _databaseVersion, onCreate: _onCreate);
+  }
+
+
+
+  Future<Database?> get database1 async
+  {
+    if (_database == null)
+    {
+      _database = await _initDatabase();
+    }
+    return _database;
+  }
+
+  Future<int>insertdata(Map<String, dynamic> row)async
+  {
+    Database? db = await instance.database;
+    return await db.insert(table, row);
+  }
+
+
+}
---
+++ Module3/13-2/widget/mydrawer.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:mydb3/screens/homescreen.dart';
+
+import '../color.dart';
+
+class MyDrawer extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+      return Container
+        (
+          margin: EdgeInsets.only(top: 55),
+          child: Drawer
+            (
+              backgroundColor: MyColors.drawalBackground,
+              child: ListView
+                (
+                  padding: EdgeInsets.zero,
+                  children:
+                  [
+                    ListTile(
+                      title: const Text(
+                        'Add Category',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onTap: ()
+                      {
+                        Navigator.push(context, MaterialPageRoute(builder: (_) => Homescreen()));
+                      },
+                    ),
+                    Divider(
+                      color: MyColors.drawalDivider,
+                      height: 2,
+                      thickness: 2,
+                    ),
+                    ListTile(
+                      title: const Text(
+                        'Add Contact',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onTap: ()
+                      {
+                        //Navigator.push(context, MaterialPageRoute(builder: (_) => HomePage()));
+                      },
+                    ),
+                    Divider(
+                      color: MyColors.drawalDivider,
+                      height: 2,
+                      thickness: 2,
+                    ),
+                    ListTile(
+                      title: const Text(
+                        'Contact List',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onTap: ()
+                      {
+                        //Navigator.push(context, MaterialPageRoute(builder: (_) => HomePage()));
+                      },
+                    ),
+                    Divider(
+                      color: MyColors.drawalDivider,
+                      height: 2,
+                      thickness: 2,
+                    ),
+
+                  ],
+                ),
+            ),
+        );
+  }
+
+}
